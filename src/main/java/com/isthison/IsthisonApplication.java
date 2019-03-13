14f92937c3549390324ed21791600f6e7a57ab0b
Matt Cadorette <mcadorette@gmail.com>
2019-03-13 13:28:55 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/isthison/IsthisonApplication.java
@@ -0,0 +1,12 @@
+package com.isthison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsthisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsthisonApplication.class, args);
+	}
+}
